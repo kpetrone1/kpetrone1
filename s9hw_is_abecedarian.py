bab55acb2dad68b81619017cc04c99795b69eeac
kpetrone1 <kpetrone1@babson.edu>
2016-09-30 23:56:40 -0400

    Homework Following Session Nine

---
+++ s9hw_is_abecedarian.py
@@ -0,0 +1,31 @@
+def is_abecedarian(word):
+    previous = word[0]
+    for c in word:
+        if c < previous:
+            return False
+        previous = c
+    return True
+
+#Exercise 2
+#1.
+
+def is_abecedarian(word, .flag, n+1):
+    previous = word[0]
+    for c in range(start, n+1):
+        if c < previous in word:
+            result = word
+            return False
+        previous += word
+    return True
+
+#2.
+
+def is_abecedarian(word):
+    previous = word[0]
+    while c < previous:
+        for c in range word:
+            previous = word[n-1]
+        return False
+    return True
+
+#Exercise 3
