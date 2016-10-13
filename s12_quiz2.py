9fbf16c74f31031a7c879de59e9c42203bd0cd4e
kpetrone1 <kpetrone1@babson.edu>
2016-10-13 13:46:51 -0400

    Quiz2_Attempt1

--- s12_quiz2.py
+++ s12_quiz2.py
@@ -56,7 +56,7 @@ def insert_integer(data, number):
 # data = [1, 3, 40, 75, 90, 2000, 2001, 2016]
 # new_data = insert_integer(data, 2015)
 # print(new_data)
-
+ 
 
 def print_hist(data):
     '''
@@ -79,3 +79,4 @@ def print_hist(data):
     letters_cpounts = 
     for letter in letters_count:
         print(letter ':', ('*' * letters_count[letter]))
+(I made a change.)
